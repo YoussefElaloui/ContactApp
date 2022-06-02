db734344bec06c4a3102869006b66e72a4aee255
YoussefElaloui <youss.2003ala@gmail.com>
2022-06-02 21:13:08 +0100

    Oppai therdo Stipo

--- app/src/main/java/com/example/contactapp/Details.java
+++ app/src/main/java/com/example/contactapp/Details.java
@@ -3,12 +3,32 @@ package com.example.contactapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Details extends AppCompatActivity {
+    RadioGroup rg;
+    ImageView iv;
+    TextView t_name,t_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        rg=findViewById(R.id.rg);
+        iv=findViewById(R.id.iv);
+        t_name=findViewById(R.id.t_name);
+        t_number=findViewById(R.id.t_number);
+
+        Contact c=(Contact)getIntent().getExtras().get("contact");
+        iv.setImageResource(c.getPicture());
+        if(c.getGender().equals("female"))
+            rg.check(R.id.rb_female);
+        else
+            rg.check(R.id.rb_male);
+        t_name.setText(c.getName());
+        t_number.setText(c.getNumber());
+
     }
 }
--- app/src/main/java/com/example/contactapp/MainActivity.java
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lst.findViewById(R.id.lst);
+        lst=findViewById(R.id.lst);
         db=new Db(this);
 
 

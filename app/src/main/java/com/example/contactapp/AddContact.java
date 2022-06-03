c711574dd4dc728c729993a76c558619735ef419
YoussefElaloui <youss.2003ala@gmail.com>
2022-06-03 01:24:01 +0100

    Zaido's Oppayo begero thano tsunadiyo

--- app/src/main/java/com/example/contactapp/AddContact.java
+++ app/src/main/java/com/example/contactapp/AddContact.java
@@ -3,12 +3,56 @@ package com.example.contactapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class AddContact extends AppCompatActivity {
+    EditText name,number;
+    RadioGroup rg;
+    Button btn_save,btn_cancel;
+    Db db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contact);
+        db=new Db(this);
+        name=findViewById(R.id.tt_name);
+        number=findViewById(R.id.tt_number);
+        rg=findViewById(R.id.rdg);
+        btn_save=findViewById(R.id.b_eng);
+        btn_cancel=findViewById(R.id.b_cnl);
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btn_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Contact c=new Contact();
+                c.setName(name.getText().toString());
+                c.setNumber(number.getText().toString());
+                if(rg.getCheckedRadioButtonId()==R.id.rd1){
+                    c.setGender("male");
+                    c.setPicture(R.drawable.male);
+                }else{
+                    c.setGender("female");
+                    c.setPicture(R.drawable.female);
+                }
+                if(Db.insertContact(db.getWritableDatabase(),c)==-1)
+                {
+                    Toast.makeText(AddContact.this, "Cannot add data", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/contactapp/MainActivity.java
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.Toast;
@@ -18,25 +19,34 @@ public class MainActivity extends AppCompatActivity {
     ListView lst;
     Db db;
     ArrayList<Contact> cc;
+    Button btn_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lst=findViewById(R.id.lst);
+        btn_add=findViewById(R.id.btn_addNew);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,AddContact.class);
+                startActivity(i);
+            }
+        });
         db=new Db(this);
 
-
-        cc=new ArrayList<>();
-        cc.add(new Contact("Youness","male","0673284936",R.drawable.male));
-        cc.add(new Contact("hmido","male","0602884936",R.drawable.male));
-        cc.add(new Contact("kaido","male","0013700936",R.drawable.male));
-        cc.add(new Contact("podingo","female","31903847",R.drawable.female));
-        cc.add(new Contact("pinasu","male","47403403",R.drawable.male));
-        cc.add(new Contact("omanko","female","077393622",R.drawable.female));
-        cc.add(new Contact("opaiyo","female","1102472",R.drawable.female));
-        for (Contact c:cc)
-            if (Db.insertContact(db.getWritableDatabase(),c)==-1)
-            Toast.makeText(this, "Cannot insert data into database", Toast.LENGTH_LONG).show();
+//
+//        cc=new ArrayList<>();
+//        cc.add(new Contact("Youness","male","0673284936",R.drawable.male));
+//        cc.add(new Contact("hmido","male","0602884936",R.drawable.male));
+//        cc.add(new Contact("kaido","male","0013700936",R.drawable.male));
+//        cc.add(new Contact("podingo","female","31903847",R.drawable.female));
+//        cc.add(new Contact("pinasu","male","47403403",R.drawable.male));
+//        cc.add(new Contact("omanko","female","077393622",R.drawable.female));
+//        cc.add(new Contact("opaiyo","female","1102472",R.drawable.female));
+//        for (Contact c:cc)
+//            if (Db.insertContact(db.getWritableDatabase(),c)==-1)
+//            Toast.makeText(this, "Cannot insert data into database", Toast.LENGTH_LONG).show();
         cc=Db.getAllContact(db.getReadableDatabase());
         ArrayList<HashMap<String,Object>> data=new ArrayList<>();
         for (Contact c:cc){
@@ -59,8 +69,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(in);
             }
         });
-
-
-
     }
 }
